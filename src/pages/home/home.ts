0e17febdb77dbf5949c3006763da375fb0e301b1
OierZ <32838709+OierZ@users.noreply.github.com>
2018-05-17 12:40:49 +0200

    appversion modificado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,24 +8,32 @@ import { AppVersion } from '@ionic-native/app-version';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  appName;
+  vappName;
   packageName;
   versionCode;
   versionNumber;
 
   constructor(private appVersion: AppVersion, public navCtrl: NavController) {
-    this.appVersion.getAppName().then((ver) => {
-      this.appName = ver;
-    });
-    this.appVersion.getPackageName().then((ver) => {
-      this.packageName = ver;
-    });
-    this.appVersion.getVersionCode().then((ver) => {
-      this.versionCode = ver;
-    });
-    this.appVersion.getVersionNumber().then((ver) => {
-      this.versionNumber = ver;
-    });
+    // this.appVersion.getAppName().then((ver) => {
+    //   this.vappName = ver;
+    // });
+    // this.appVersion.getPackageName().then((ver) => {
+    //   this.packageName = ver;
+    // });
+    // this.appVersion.getVersionCode().then((ver) => {
+    //   this.versionCode = ver;
+    // });
+    // this.appVersion.getVersionNumber().then((ver) => {
+    //   this.versionNumber = ver;
+    // });
+
+    this.vappName = this.appVersion.getAppName();
+
+    this.packageName = this.appVersion.getPackageName();
+
+    this.versionCode = this.appVersion.getVersionCode();
+
+    this.versionNumber = this.appVersion.getVersionNumber();
 
     Pro.monitoring.exception(new Error('Error Entrar App'));
 
